a84daeea727de919d14e89bcf45412a44bd6a4c5
abhi269 <katyan.abhishek@gmail.com>
2019-03-24 20:18:41 +0530

    forget password

--- ce_online_test/settings.py
+++ ce_online_test/settings.py
@@ -113,7 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'Katyan.abhishek@gmail.com'
+EMAIL_HOST_PASSWORD = 'ngyctkyvyvbmarra'
+DEFAULT_FROM_EMAIL = 'Katyan.abhishek@gmail.com'
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
